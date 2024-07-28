dbfd38f811c908d62bb564611843f0396d30aecf
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2024-07-28 00:14:15 -0400

    button up

--- src/App.js
+++ src/App.js
@@ -9,28 +9,24 @@ function App() {
 
     const createSearchLink = () => {
         // const scryfallLink = `https://scryfall.com/search?q=${encodeURIComponent(query)}`;
-        const scryfallLink =  `${location.pathname}search?q=${encodeURIComponent(query)}`;
-        setLink(scryfallLink);
+        const searchLink =  `${location.pathname}search?q=${encodeURIComponent(query)}`;
+        setLink(searchLink);
     };
 
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Let Me Scryfall That For You...</h1>
-                <input
-                    type="text"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                    placeholder=""
-                />
-                <button onClick={createSearchLink}>Search</button>
-                {link && (
-                    <div style={{ marginTop: '20px' }}>
-                        <a href={link} target="_blank" rel="noopener noreferrer">
-                            Click here to search "{query}" on Scryfall
-                        </a>
+            <header className="App-content">
+                <h1 className="homepage-title"><strong>Scryfall</strong> is a powerful <strong>Magic: The Gathering</strong> card search</h1>
+                <p className="homepage-subtitle">For those who know how to use it...</p>
+                <form className="homepage-search" action="/search" acceptCharset="UTF-8" method="get">
+                    <div className="search-container">
+                        <svg focusable="false" aria-hidden="true" className="scryfall-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 460 460">
+                            <image href="/scryfall-logo.svg" width="100%" height="100%" />
+                        </svg>
+                        <input type="text" name="q" placeholder="Search cards..." />
                     </div>
-                )}
+                    <button type="submit">Search</button>
+                </form>
             </header>
         </div>
     );
